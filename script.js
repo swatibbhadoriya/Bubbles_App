7a07da24ab03016ff708d77b10c97898ac662b60
Swati Singh <73748705+swatibbhadoriya@users.noreply.github.com>
2023-06-11 23:02:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
 var initialCircles = [
-  { x: 80, y: 100, radius: 45, color: "#0A2647", hit: false },
-  { x: 80, y: 210, radius: 45, color: "#144272", hit: false },
-  { x: 80, y: 320, radius: 45, color: "#205295", hit: false },
-  { x: 80, y: 430, radius: 45, color: "#2C74B3", hit: false }
+    { x: 80, y: 100, radius: 45, color: "#ffd965", hit: false },
+    { x: 80, y: 210, radius: 45, color: "#2b78e4", hit: false },
+    { x: 80, y: 320, radius: 45, color: "#cc0000", hit: false },
+    { x: 80, y: 430, radius: 45, color: "#6ba84f", hit: false }
 ];
 var initialArrows = [
-  { x: 550, y: 100, hit: false, moving: false },
-  { x: 550, y: 210, hit: false, moving: false },
-  { x: 550, y: 320, hit: false, moving: false },
-  { x: 550, y: 430, hit: false, moving: false }
+    { x: 550, y: 100, hit: false, moving: false },
+    { x: 550, y: 210, hit: false, moving: false },
+    { x: 550, y: 320, hit: false, moving: false },
+    { x: 550, y: 430, hit: false, moving: false }
 ];
 var circles = [];
 var arrows = [];
@@ -21,9 +21,9 @@ var animationId;
 reset();
 
 function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  circles.forEach(function (circle) {
+circles.forEach(function (circle) {
     ctx.beginPath();
     ctx.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI);
     ctx.fillStyle = circle.color;
@@ -33,13 +33,9 @@ function draw() {
     ctx.arc(circle.x + 4, circle.y - 4, circle.radius, 0, 2 * Math.PI);
     ctx.fillStyle = "rgba(255, 255, 255, 0.2)";
     ctx.fill();
-  });
+});
 
-  arrows.forEach(function (arrow, index) {
-    if (arrow.hit) {
-      return;
-    }
-
+arrows.forEach(function (arrow, index) {
     ctx.beginPath();
     ctx.moveTo(arrow.x, arrow.y);
     ctx.lineTo(arrow.x + 40, arrow.y - 15);
@@ -67,50 +63,50 @@ function draw() {
     ctx.fill();
 
     if (arrow.moving) {
-      var circle = circles[index];
-      var speed = 5;
-      var directionX = circle.x - arrow.x;
-      var directionY = circle.y - arrow.y;
-      var distance = Math.sqrt(Math.pow(directionX, 2) + Math.pow(directionY, 2));
-      var unitDirectionX = directionX / distance;
-      var unitDirectionY = directionY / distance;
-      arrow.x += unitDirectionX * speed;
-      arrow.y += unitDirectionY * speed;
+        var circle = circles[index];
+        var speed = 5;
+        var directionX = circle.x - arrow.x;
+        var directionY = circle.y - arrow.y;
+        var distance = Math.sqrt(Math.pow(directionX, 2) + Math.pow(directionY, 2));
+        var unitDirectionX = directionX / distance;
+        var unitDirectionY = directionY / distance;
+        arrow.x += unitDirectionX * speed;
+        arrow.y += unitDirectionY * speed;
 
-      if (distance < circle.radius) {
-        arrow.moving = false;
-        arrow.hit = true;
-        circles[index].hit = true;
-        circles[index].color = "grey";
+        if (distance < circle.radius) {
+            arrow.moving = false;
+            arrow.hit = true;
+            circles[index].hit = true;
+            circles[index].color = "grey";
 
-        if (index < circles.length - 1 && circles[index + 1].color === "grey") {
-          arrows[index + 1].hit = true;
-          circles[index + 1].color = "grey";
+            if (index < circles.length - 1 && circles[index + 1].color === "grey") {
+                arrows[index + 1].hit = true;
+                circles[index + 1].color = "grey";
+            }
         }
-      }
     }
-  });
+});
 
-  animationId = requestAnimationFrame(draw);
+animationId = requestAnimationFrame(draw);
 }
 
 function handleClick(event) {
-  var rect = canvas.getBoundingClientRect();
-  var mouseX = event.clientX - rect.left;
-  var mouseY = event.clientY - rect.top;
+    var rect = canvas.getBoundingClientRect();
+    var mouseX = event.clientX - rect.left;
+    var mouseY = event.clientY - rect.top;
 
-  circles.forEach(function (circle, index) {
-    if (!circle.hit) {
-      var distance = Math.sqrt(Math.pow(mouseX - circle.x, 2) + Math.pow(mouseY - circle.y, 2));
-      if (distance < circle.radius) {
-        var arrow = arrows[index];
+    circles.forEach(function (circle, index) {
+        if (!circle.hit) {
+            var distance = Math.sqrt(Math.pow(mouseX - circle.x, 2) + Math.pow(mouseY - circle.y, 2));
+            if (distance < circle.radius) {
+                var arrow = arrows[index];
 
-        if (!arrow.moving) {
-          arrow.moving = true;
+                if (!arrow.moving) {
+                    arrow.moving = true;
+                }
+            }
         }
-      }
-    }
-  });
+    });
 }
 
 canvas.addEventListener("click", handleClick);
@@ -121,22 +117,22 @@ resetButton.addEventListener("click", reset);
 animationId = requestAnimationFrame(draw);
 
 function reset() {
-  cancelAnimationFrame(animationId);
+    cancelAnimationFrame(animationId);
 
-  circles = JSON.parse(JSON.stringify(initialCircles));
-  arrows = JSON.parse(JSON.stringify(initialArrows));
+    circles = JSON.parse(JSON.stringify(initialCircles));
+    arrows = JSON.parse(JSON.stringify(initialArrows));
 
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  animationId = requestAnimationFrame(draw);
+    animationId = requestAnimationFrame(draw);
 }
 
 function allCirclesHit() {
-  return circles.every(function (circle) {
-    return circle.hit;
-  });
+    return circles.every(function (circle) {
+        return circle.hit;
+    });
 }
 
 if (allCirclesHit()) {
-  reset();
+    reset();
 }
